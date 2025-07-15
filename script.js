68a962a3a9b16c58088549813a1b1ab5df9c1745
ChinazaOzor <judithmary172@gmail.com>
2025-07-15 18:16:18 +0100

    Second commit message

--- script.js
+++ script.js
@@ -1,130 +1,91 @@
 //Part two:
 
-//This is for const:
-// const intern = "Chinaza"
-// console.log(intern)
+//(a) This is for const:
+// const age = 24;
+// console.log(age)
 
- //This is for let:
-// let intern = "Ozor"
-// console.log(intern)
+//(a) This is for let:
+// let email = th@gmail.com;
+// console.log(email)
 
-//This is for var:
-// var intern = "Judith"
-// console.log(intern)
+//(a) This is for var:
+// var result = 50
+// console.log(result)
 
-// //For total score:
-//let scores = [10, 8, 6]
-// calculate total score:
-//  let total = 0;
-//  for(let i = 0; i<scores.length; i++)
-// {
-//      total += scores[i];
+//(b) intern name:
+// let intern name = "Chinaza"
+// console.log(intern name)
+
+//(c) scores for task 1, 2 and 3:
+ let scores = [10, 8, 6]
+ console.log(scores)
+
+//No 2:
+//For total score:
+
+// let total = 0;
+//   for(let i = 0; i<scores.length; i++)
+//  {
+//       total += scores[i];
 //  }
 //  console.log("total score:", total);
 
-// For average score:
-// let average = total / scores.length;
+//  //For average score:
+//  let average = total / scores.length;
 //  console.log("average score:", average);
 
-// let scores = [10, 8, 6];
-// let average = scores.reduce((sum, score) => sum + score, 0) / scores.length;
+
 
 
 // //No 3, To determine grade using if-else:
-// let gradeA = "A";
-// let gradeB = "B";
-// let gradeC = "C";
-// let gradeD = "D";
-// let gradeE = "E";
 
-
-// let grade;
-// if (average >= 9){
-//     grade = gradeA;
-// }
-// else if (average >= 8){
-//     grade = gradeB;
-// }
-// else if (average >= 7){
-//     grade = gradeC;
+// const average = 8;
+// if(average >= 9){
+//   console.log("A")
+// }else if(average >= 8){
+//   console.log("B")
+// }else if(average >= 7){
+//   console.log("C")
+// }else if(average >= 6){
+//    console.log("D")
+// }else{
+//   console.log("E")
 // }
 
-// else if (average >= 6){
-//     grade = gradeD;
-// }
-// else{
-//     grade = gradeE;
-// }
 
-// console.log("average:", average.toFixed(2));
-// console.log("Grade:", grade);
+// //No 4:
+// const studentName = "Amanda";
+// let eng = 10;
+// let math = 8;
+// let history = 9;
+// let average = 21.66;
+// let finalGrade = 27;
+// let message = (`${studentName}, you sored ${math} in math, ${eng} in english, and ${history} in history. Congratulations! your final grade is ${finalGrade}, while your average score is ${average}.`)
+// console.log(message)
 
-//No 4:
-const studentName = "Mary";
-const scores = {
-Math: 9,
-English: 8,
-Science: 7
-};
-
-//Average:
-const scoreValues = Object.values(scores);
-const total = scoreValues.reduce((sum, val) => sum + val, 0);
-const average = (total/ scoreValues.length).toFixed(2);
-
-//Determine grade:
-// let grade;
-// let message;
-
-// if(average >=9){
-//     grade = "A";
-//     message = "Excellent work!";
-// }
-// else if(average >= 8){
-//     grade = "B";
-//     message = "Great job!";
-// }
-//  else if (average >= 7) {
-//   grade = "C";
-//   message = "Good effort!";
-// } else if (average >= 6) {
-//   grade = "D";
-//   message = "Needs improvement.";
-// } else {
-//   grade = "E";
-//   message = "Try harder next time.";
-// }
-
-// console.log(`StudentName: ${studentName}`);
-// console.log("Subject Scores:");
-// for (const subject in scores) {
-//   console.log(`- ${subject}: ${scores[subject]}`);
-// }
-// console.log(`average Score: ${average}`);
-// console.log(`Final Grade: ${grade}`);
-// console.log(`Message: ${message}`);
 
 //No 5:
-const grade = "A"; 
-let message;
 
-switch (grade) {
-  case "A":
-    message = "Excellent performance";
-    break;
-  case "B":
-    message = "Great job! Keep aiming high";
-    break;
-  case "C":
-    message = "Good effort. Try to improve.";
-    break;
-  case "D":
-  case "E":
-    message = "Don't give up. You can do better next time.";
-    break;
-  default:
-    message = "Invalid grade";
-}
+// const grade = "A"; 
+// let message;
 
-console.log(`Grade: ${grade}`);
-console.log(`Feedback: ${message}`);
+// switch (grade) {
+//   case "A":
+//     message = "Excellent performance";
+//     break;
+//   case "B":
+//     message = "Great job! Keep aiming high";
+//     break;
+//   case "C":
+//     message = "Good effort. Try to improve.";
+//     break;
+//   case "D":
+//   case "E":
+//     message = "Don't give up. You can do better next time.";
+//     break;
+//   default:
+//     message = "Invalid grade";
+// }
+
+// console.log(`Grade: ${grade}`);
+// console.log(`Feedback: ${message}`);
